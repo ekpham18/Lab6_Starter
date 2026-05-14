3ba2cd195f53e6ef8ae4fe46504b4a4d9f86e599
Ethan Pham <ekpham@ucsd.edu>
2026-05-14 10:03:00 -0700

    fix image not loading

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -130,8 +130,8 @@ class RecipeCard extends HTMLElement {
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
 		articleEl.innerHTML = `
-  <img src=${data.imgSrc}
-    alt=${data.imgAlt}>
+  <img src="${data.imgSrc}"
+    alt="${data.imgAlt}">
   <p class="title">
     <a href="${data.titleLnk}">${data.titleTxt}</a>
   </p>
